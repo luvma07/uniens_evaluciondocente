3d22277bf02fe1b44716fefd8225686646ab06d1
roilhi <roilhi@gmail.com>
2021-12-07 19:38:23 -0800

    Correccion typos preguntas

---
+++ controllers/seleccionar_cuestionario4.php
@@ -0,0 +1,14 @@
+<?php
+
+    $pagina = 2;
+    setcookie("paginacion", $pagina, time() + 60 * 30, "/EvaluacionDocente");
+
+    $cuestionario = 1;
+    setcookie("cuestionario", $cuestionario, time() + 60 * 30, "/EvaluacionDocente");
+
+    //if ($cuestionario == 0) {
+    //    $cuestionario_curso = 3;
+    //    setcookie("cuestionario_docente", $cuestionario_curso, time() + 60 * 30, "/EvaluacionDocente");
+    //}
+
+    header("Location:../index.php");
